8351e5d413794722fba1acffa8e4c8e211b9aa79
Binaya Karki <Binay7587@gmail.com>
2023-02-05 17:40:32 +0545

    Feat: Active, completed task added with clear all feature

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react'
-import { Container, Form, Button, Alert, Row, Col } from 'react-bootstrap';
+import { Form, Button } from 'react-bootstrap';
 import "bootstrap/dist/css/bootstrap.css";
 import { FaPlus, FaTrash } from "react-icons/fa"
+import './App.css';
 
 const App = () => {
     const initialData = localStorage.getItem("todos") ? JSON.parse(localStorage.getItem("todos")) : [];
@@ -11,7 +12,7 @@ const App = () => {
     // Function to add todo item
     const addTodo = () => {
         const todos = [
-            ...todoList,
+            ...localStorage.getItem("todos") ? JSON.parse(localStorage.getItem("todos")) : [],
             {
                 data: text,
                 date: new Date().toLocaleString().split(",")[0],
@@ -26,7 +27,8 @@ const App = () => {
 
     // Function to toggle todo item completion
     const toggleTodoCompletion = (idx) => {
-        const updatedTodo = todoList.map((todo, index) => index === idx ? { ...todo, isCompleted: !todo.isCompleted } : todo);
+        const todos = localStorage.getItem("todos") ? JSON.parse(localStorage.getItem("todos")) : [];
+        const updatedTodo = todos.map((todo, index) => index === idx ? { ...todo, isCompleted: !todo.isCompleted } : todo);
         setTodoList(updatedTodo);
         localStorage.setItem("todos", JSON.stringify(updatedTodo));
     }
@@ -43,45 +45,89 @@ const App = () => {
         }
     }
 
+    // Function to filter todo items
+    const filterTodo = (type) => {
+        const filters = document.getElementsByClassName('filters')[0].children;
+        for (let i = 0; i < filters.length; i++) {
+            filters[i].classList.remove('active');
+        }
+
+        if (type === 'active') {
+            filters[1].classList.add('active');
+            const todos = localStorage.getItem("todos") ? JSON.parse(localStorage.getItem("todos")) : [];
+            const activeTodos = todos.filter(todo => !todo.isCompleted);
+            setTodoList(activeTodos);
+        } else if (type === 'completed') {
+            filters[2].classList.add('active');
+            const todos = localStorage.getItem("todos") ? JSON.parse(localStorage.getItem("todos")) : [];
+            const completedTodos = todos.filter(todo => todo.isCompleted);
+            setTodoList(completedTodos);
+        } else if (type === 'all') {
+            filters[0].classList.add('active');
+            const todos = localStorage.getItem("todos") ? JSON.parse(localStorage.getItem("todos")) : [];
+            setTodoList(todos);
+        }
+    }
+
+    //Function to show hide trash icon
+    const showHideTrash = (idx, type) => {
+        document.getElementById(`trash${idx}`).firstChild.style.display = type;
+    }
+
+    //Function to clear all todos
+    const clearAll = () => {
+        const confirm = window.confirm("Do you want to clear all todos? ");
+        if (confirm) {
+            setTodoList([]);
+            localStorage.setItem("todos", JSON.stringify([]));
+        }
+    }
+
     return (
-        <Container className='mt-3 text-center'>
-            <h3>TodoList App</h3>
-            <Form.Control
-                type="text"
-                value={text}
-                onChange={(e) => setText(e.target.value)}
-                onKeyDown={(e) => e.key === "Enter" && addTodo()}
-            />
-            <br />
-            <Button onClick={addTodo}>
-                {" "}
-                <FaPlus />
-                <label className='ms-2'>Add</label>
-            </Button>
-            <br />
-            {todoList.length > 0 ? todoList.map((todo, index) => {
-                return (
-                    <Row key={index}>
-                        <Col xs={10}>
-                            <Alert
-                                variant={todo.isCompleted ? "danger" : "primary"}
-                                className="text-start mt-3"
-                                style={{
+        <div className='container'>
+            <h3>Todo List</h3>
+            <div className="inputField">
+                <Form.Control
+                    type="text"
+                    value={text}
+                    placeholder="Enter new Todo"
+                    onChange={(e) => setText(e.target.value)}
+                    onKeyDown={(e) => e.key === "Enter" && addTodo()}
+                />
+                <Button onClick={addTodo}>
+                    <FaPlus />
+                </Button>
+            </div>
+            <div className='controls'>
+                <div className='filters'>
+                    <span onClick={() => filterTodo('all')} className={'active'}>All</span>
+                    <span onClick={() => filterTodo('active')}>Active</span>
+                    <span onClick={() => filterTodo('completed')}>Completed</span>
+                </div>
+                <Button variant="danger" onClick={clearAll}>Clear All</Button>
+            </div>
+            <ul className='todo-list'>
+                {todoList.length > 0 ? todoList.map((todo, index) => {
+                    return (
+                        <li key={index} className='task' onClick={() => toggleTodoCompletion(index)} onMouseEnter={() => showHideTrash(index, 'block')} onMouseLeave={() => showHideTrash(index, 'none')}>
+                            <div>
+                                <span className='task-title' style={{
+                                    textDecoration: todo.isCompleted ? "line-through" : "none"
+                                }}>{todo.data}</span><br />
+                                <small className='task-date' style={{
                                     textDecoration: todo.isCompleted ? "line-through" : "none",
-                                    cursor: "pointer"
-                                }}
-                                onClick={() => toggleTodoCompletion(index)}>
-                                <h3>{todo.data}</h3>
-                                <small>{todo.date}</small>
-                            </Alert>
-                        </Col>
-                        <Col className='mt-4'>
-                            <FaTrash size="40" color="red" onClick={() => deleteTodo(index)} />
-                        </Col>
-                    </Row>
-                )
-            }) : "No Todo's"}
-        </Container>
+                                }}>{todo.date}</small>
+                            </div>
+                            <div id={`trash${index}`}>
+                                <FaTrash size="40" color="red" display={'none'} onClick={() => {
+                                    deleteTodo(index)
+                                }} />
+                            </div>
+                        </li >
+                    )
+                }) : <li className='task'>No Any Todo ...</li>}
+            </ul>
+        </div >
     )
 }
 
