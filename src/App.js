ece7a07fa877256cc838454a4ff07f801d406245
Binaya Karki <Binay7587@gmail.com>
2023-02-07 10:07:52 +0545

    Refactor: code optimization

--- src/App.js
+++ src/App.js
@@ -7,113 +7,141 @@ import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
 
 const App = () => {
-    const initialData = localStorage.getItem("todos") ? JSON.parse(localStorage.getItem("todos")) : [];
-    const [todoList, setTodoList] = useState(initialData);
+    /**
+    * @description Function to get local storage data
+    * @param {string} key     * 
+    */
+    const getLocalStorageData = (key) => {
+        return localStorage.getItem(key) ? JSON.parse(localStorage.getItem(key)) : [];
+    }
+
+    /**
+     * @description Function to set local storage data
+     * @param {string} key
+     * @param {object} data
+     */
+    const setLocalStorageData = (key, data) => {
+        return localStorage.setItem(key, JSON.stringify(data));
+    }
+
+    const [todoList, setTodoList] = useState(getLocalStorageData("todos"));
     const [text, setText] = useState("");
 
-    // Function to add todo item
+    /**
+     * @description Function to add todo item
+     */
     const addTodo = () => {
         if (!text) {
             notify("Please enter todo.", 'error');
             return;
         }
-        const todos = [
-            ...localStorage.getItem("todos") ? JSON.parse(localStorage.getItem("todos")) : [],
-            {
-                data: text,
-                date: new Date().toLocaleString().split(",")[0],
-                isCompleted: false
-            }
-        ];
-        setTodoList(todos);
-        setText("");
+        setTodoList((prev) => {
+            const newList = [
+                ...prev,
+                {
+                    data: text,
+                    date: new Date().toLocaleString().split(",")[0],
+                    isCompleted: false
+                }
+            ];
 
-        localStorage.setItem("todos", JSON.stringify(todos));
+            setLocalStorageData("todos", newList);
+            return newList;
+        });
+
+        setText("");
         notify("Todo added successfully.", 'success');
     };
 
-    // Function to toggle todo item completion
+    /**
+     * @description Function to toggle todo item completion
+     * @param {number} idx 
+     */
     const toggleTodoCompletion = (idx) => {
-        const todos = localStorage.getItem("todos") ? JSON.parse(localStorage.getItem("todos")) : [];
-        const updatedTodo = todos.map((todo, index) => index === idx ? { ...todo, isCompleted: !todo.isCompleted } : todo);
-        setTodoList(updatedTodo);
-        localStorage.setItem("todos", JSON.stringify(updatedTodo));
+        setTodoList((prev) => {
+            const updatedTodo = prev.map((todo, index) => index === idx ? { ...todo, isCompleted: !todo.isCompleted } : todo);
+            setLocalStorageData("todos", updatedTodo);
+            return updatedTodo;
+        });
     }
 
-    // Function to delete todo item
+    /**
+     * Function to delete todo item
+     * @param {number} idx
+     */
     const deleteTodo = (idx) => {
         const confirm = window.confirm("Do you want to delete? ");
         if (confirm) {
-            const updatedTodo = todoList.filter((_, index) => {
-                return index === idx ? false : true;
-            })
+            setTodoList((prev) => {
+                const updatedTodo = prev.filter((_, index) => index !== idx);
+                setLocalStorageData("todos", updatedTodo);
+                return updatedTodo;
+            });
             notify("Todo deleted successfully.", 'success');
-            setTodoList(updatedTodo);
-            localStorage.setItem("todos", JSON.stringify(updatedTodo));
         }
     }
 
-    // Function to filter todo items
+    /**
+     * Function to filter todo items
+     * @param {string} type
+     */
     const filterTodo = (type) => {
         const filters = document.getElementsByClassName('filters')[0].children;
         for (let i = 0; i < filters.length; i++) {
             filters[i].classList.remove('active');
         }
 
+        const todos = getLocalStorageData("todos");
         if (type === 'active') {
             filters[1].classList.add('active');
-            const todos = localStorage.getItem("todos") ? JSON.parse(localStorage.getItem("todos")) : [];
-            const activeTodos = todos.filter(todo => !todo.isCompleted);
-            setTodoList(activeTodos);
+            const activeTodo = todos.filter(todo => !todo.isCompleted);
+            setTodoList(activeTodo);
         } else if (type === 'completed') {
             filters[2].classList.add('active');
-            const todos = localStorage.getItem("todos") ? JSON.parse(localStorage.getItem("todos")) : [];
-            const completedTodos = todos.filter(todo => todo.isCompleted);
-            setTodoList(completedTodos);
+            const activeTodo = todos.filter(todo => todo.isCompleted);
+            setTodoList(activeTodo);
         } else if (type === 'all') {
             filters[0].classList.add('active');
-            const todos = localStorage.getItem("todos") ? JSON.parse(localStorage.getItem("todos")) : [];
             setTodoList(todos);
         }
     }
 
-    //Function to show hide trash icon
+    /**
+     * @description Function to show hide trash icon
+     * @param {number} idx 
+     * @param {string} type 
+     */
     const showHideTrash = (idx, type) => {
         document.getElementById(`trash${idx}`).firstChild.style.display = type;
     }
 
-    //Function to clear all todos
+    /**
+     * @description Function to clear all todos
+     */
     const clearAll = () => {
         const confirm = window.confirm("Do you want to clear all todos? ");
         if (confirm) {
             setTodoList([]);
-            localStorage.setItem("todos", JSON.stringify([]));
+            setLocalStorageData("todos", []);
             notify("All todos cleared successfully", 'success');
         }
     }
 
+    /**
+     * @description Function to show toast notification
+     * @param {string} message 
+     * @param {string} type 
+     */
     const notify = (message, type) => {
-        if (type === 'success') {
-            toast.success(message, {
-                position: "bottom-right",
-                autoClose: 2000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                progress: undefined,
-            });
-        } else {
-            toast.error(message, {
-                position: "bottom-right",
-                autoClose: 2000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                progress: undefined,
-            });
-        }
+        toast[type === "success" ? "success" : "error"](message, {
+            position: "bottom-right",
+            autoClose: 2000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+        });
     };
 
     return (
